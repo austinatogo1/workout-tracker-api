d3ac14b4fe8786159aff1b836d971584f8d14f9a
austinatogo1 <austin.atogo1@student.moringaschool.com>
2026-08-29 20:44:45 +0300

    feat: add Exercise model

--- server/models.py
+++ server/models.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Exercise {self.id}: {self.name}>'
