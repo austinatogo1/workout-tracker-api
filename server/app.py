1d530dddf6d0d466d83ebffb9f35a3813aede503
austinatogo1 <austin.atogo1@student.moringaschool.com>
2026-08-29 21:07:06 +0300

    feat: configure database migrations

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 from models import db
 
@@ -9,6 +11,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    """Enable SQLite foreign key enforcement, which is off by default."""
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
