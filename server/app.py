bce62a8b9e2ff4dcfe1cade11859f6231d221e01
austinatogo1 <austin.atogo1@student.moringaschool.com>
2026-08-29 21:25:15 +0300

    feat: implement GET /workouts endpoint

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 
-from models import db
+from models import db, Workout
+from schemas import WorkoutSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -12,6 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
+workout_schema = WorkoutSchema()
+workouts_schema = WorkoutSchema(many=True)
+
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -19,6 +23,12 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
+
+
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+    workouts = Workout.query.all()
+    return jsonify(workouts_schema.dump(workouts)), 200
 
 
 if __name__ == '__main__':
