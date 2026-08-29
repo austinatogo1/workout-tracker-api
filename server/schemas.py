1d610e936cff986af901575f6a5aee161add604a
austinatogo1 <austin.atogo1@student.moringaschool.com>
2026-08-29 21:17:12 +0300

    feat: add nested serialization for workout and exercise detail views

--- server/schemas.py
+++ server/schemas.py
@@ -13,3 +13,24 @@ class WorkoutSchema(Schema):
     date = fields.Date(required=True)
     duration_minutes = fields.Integer(required=True)
     notes = fields.String()
+
+
+class WorkoutExerciseSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    reps = fields.Integer()
+    sets = fields.Integer()
+    duration_seconds = fields.Integer()
+    exercise = fields.Nested(ExerciseSchema, dump_only=True)
+    workout = fields.Nested(WorkoutSchema, dump_only=True)
+
+
+class WorkoutDetailSchema(WorkoutSchema):
+    workout_exercises = fields.Nested(
+        WorkoutExerciseSchema, many=True, dump_only=True, exclude=('workout',)
+    )
+
+
+class ExerciseDetailSchema(ExerciseSchema):
+    workout_exercises = fields.Nested(
+        WorkoutExerciseSchema, many=True, dump_only=True, exclude=('exercise',)
+    )
